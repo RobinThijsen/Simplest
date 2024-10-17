2e53cf6fe1a5b9b09f219d7c48cad624ca2e37f3
Robin <robin.t@live.be>
2024-10-17 21:56:09 +0200

    fix Modal class component

--- src/Views/Components/Modal.php
+++ src/Views/Components/Modal.php
@@ -7,10 +7,12 @@ use Illuminate\View\Component;
 class Modal extends Component
 {
     public string $name;
+    public string $display;
 
-    public function __construct(string $name)
+    public function __construct(string $name, string $display = 'block')
     {
         $this->name = $name;
+        $this->display = $display;
     }
 
     /**
